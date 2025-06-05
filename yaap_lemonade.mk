2a5ba7161d71c945536caa45b2c19bfad66bb038
John Galt <johngaltfirstrun@gmail.com>
2025-06-05 10:16:30 -0400

    no effort lineage->yaap

--- yaap_lemonade.mk
+++ yaap_lemonade.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonade device
 $(call inherit-product, device/oneplus/lemonade/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_lemonade
 PRODUCT_DEVICE := lemonade
